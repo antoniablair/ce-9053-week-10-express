242bd62612b31ac6d4af0d92042202a83b85f4ea
Antonia Blair <antoniablair@gmail.com>
2015-07-29 23:44:04 +0000

    Adds simplistic controllers

--- public/app/app.js
+++ public/app/app.js
@@ -7,3 +7,19 @@
 // If you have no dependencies you need to put in an empty []
 // or you will get an error "myworld cannot be found"
 var app = angular.module("myWorld", []);
+
+// everytime you use a controller that function is instantiated
+app.controller("MyCtrl", function($scope){
+    console.log("I am a controller");
+    $scope.name = "professor";
+    console.log($scope);
+});
+
+// the way that data is passed to a controller is through scope
+// there's ab object in angular to grab a window object, change the http request, etc
+
+// when the angular callback function myCtrl is called, it will pass us a 
+// scope object. We can pass data to the scope object and that data is 
+// available via the view.
+
+// two way data binding - you're changing something client and server side.
