7adbca6ebabdce28ad06848d43adcf74f2b9a01f
Antonia Blair <antoniablair@gmail.com>
2015-07-29 23:11:36 +0000

    Updates

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.set("view engine", "jade");
 // Use this app.use as middleware. Middleware has to be a function. A next 
 // parameter is being called somewhere.
 
+// If you're using app.use, the thing that you have to pass to it is a function
+
 // A higher order function is a function used as middleware (they take another
 // function or return another function) -- you know it's returning another
 // function because otherwise it won't work. It's calling next
